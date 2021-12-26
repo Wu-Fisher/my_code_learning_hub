1ce94d09e28c1cf090edfc55aca7e48def9044bd
wufisher <wufisher>
2021-12-26 10:08:37 +0800

    m

--- java/Solution1078.java
+++ java/Solution1078.java
@@ -1,6 +1,17 @@
+package hub_repo.my_leetcode_learning_hub.java;
 
-class Solution {
+import java.util.ArrayList;
+
+class Solution1078 {
     public String[] findOcurrences(String text, String first, String second) {
-
+            String[] ss = text.split(" ");
+            int n=ss.length;
+            ArrayList<String> sb = new ArrayList<>();
+            for(int i=0;i+2<n;i++)
+            {
+                if(ss[i].equals(first)&&ss[i+1].equals(second))
+                    sb.add(ss[i+2]);
+            }
+            return sb.toArray(new String[sb.size()]);
     }
 }
