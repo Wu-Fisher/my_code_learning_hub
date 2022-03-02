d5456aa602d8a61ee9bf06b71daf5a259a032a93
Wt <1105889424@qq.com>
2022-03-02 13:18:35 +0800

    Toda3/2

---
+++ java/Solution537.java
@@ -0,0 +1,15 @@
+public class Solution537 {
+    public String complexNumberMultiply(String num1, String num2) {
+        String[] ss1 = num1.split("\\+|i");
+        String[] ss2 = num2.split("\\+|i");
+        int a = Integer.parseInt(ss1[0]);
+        int b = Integer.parseInt(ss1[1]);
+        int c = Integer.parseInt(ss2[0]);
+        int d = Integer.parseInt(ss2[1]);
+
+        int A = a * c - b * d;
+        int B = b * c + a * d;
+        return A + "+" + B + "i";
+
+    }
+}
