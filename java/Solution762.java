7c2193d0fc7ccf66320a72deafd2f4d9dff59ecd
Wt <1105889424@qq.com>
2022-04-05 11:45:11 +0800

    Today4/5

--- java/Solution762.java
+++ java/Solution762.java
@@ -1,6 +1,6 @@
-public class Solution762
-static boolean[] hash = new boolean[40];
-static {
+public class Solution762 {
+    static boolean[] hash = new boolean[40];
+    static {
         int[] nums = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 };
         for (int x : nums)
             hash[x] = true;
@@ -8,16 +8,15 @@ static {
     }
 
     public int countPrimeSetBits(int left, int right) {
-        int ans=0;
-        for(int i=left;i<=right;i++){
-            int cnt=0,x=i;
-            while(x>0&&++cnt>=0)
-            {
-                x-=x&-x;
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            int cnt = 0, x = i;
+            while (x > 0 && ++cnt >= 0) {
+                x -= x & -x;
             }
-            if(hash[cnt])
+            if (hash[cnt])
                 ans++;
-    }
-    return ans;
+        }
+        return ans;
     }
 }
