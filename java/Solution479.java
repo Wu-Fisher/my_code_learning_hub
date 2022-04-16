0c104d818d896b708821493a0211758fc6b4f1bb
Wt <1105889424@qq.com>
2022-04-16 17:59:38 +0800

    Today4.16

---
+++ java/Solution479.java
@@ -0,0 +1,19 @@
+public class Solution479 {
+    public int largestPalindrome(int n) {
+        if (n == 1)
+            return 9;
+        int max = (int) Math.pow(10, n) - 1;
+        for (int i = max; i >= 0; i--) {
+            long num = i, t = i;
+            while (t > 0) {
+                num = num * 10 + t % 10;
+                t /= 10;
+            }
+            for (long j = max; j * j > num; j--) {
+                if (num % j == 0)
+                    return (int) (num % 1337);
+            }
+        }
+        return -1;
+    }
+}
