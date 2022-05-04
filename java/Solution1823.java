5037d920055a50f0ef925ba9ce9af3674f697591
Wt <1105889424@qq.com>
2022-05-04 10:09:12 +0800

    Today

---
+++ java/Solution1823.java
@@ -0,0 +1,9 @@
+public class Solution1823 {
+    public int findTheWinner(int n, int k) {
+        int p = 0;
+        for (int i = 2; i <= n; i++) {
+            p = (p + k) % i;
+        }
+        return p + 1;
+    }
+}
