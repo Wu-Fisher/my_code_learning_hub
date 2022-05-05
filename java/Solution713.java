32c54986a479c8a9b6206263a1c51464dffaa264
Wt <1105889424@qq.com>
2022-05-05 10:07:51 +0800

    today

---
+++ java/Solution713.java
@@ -0,0 +1,16 @@
+public class Solution713 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n = nums.length;
+        int ans = 0;
+        if (k <= 1)
+            return 0;
+        for (int i = 0, j = 0, cur = 1; i < n; i++) {
+            cur *= nums[i];
+            while (cur >= k) {
+                cur /= nums[j++];
+            }
+            ans += i - j + 1;
+        }
+        return ans;
+    }
+}
