7f5465e962da5bc923da7ce87a6d061bc46d9e7a
Wufisher <1105889424@qq.com>
2022-06-08 09:02:10 +0800

    Today

---
+++ java/Solution1037.java
@@ -0,0 +1,5 @@
+public class Solution1037 {
+    public boolean isBoomerang(int[][] p) {
+        return (p[1][0] - p[0][0]) * (p[2][1] - p[0][1]) != (p[1][1] - p[0][1]) * (p[2][0] - p[0][0]);
+    }
+}
